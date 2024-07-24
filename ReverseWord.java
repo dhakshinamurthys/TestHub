b1807d4cdbcc11db0ef930cf5aa2248fa2e8db95
dhakshinamurthys <murthy.dhakshina24@gmail.com>
2024-07-24 13:16:19 +0530

    [#RT-I3186] Update ReverseWord.java
    For testing purpose

--- ReverseWord.java
+++ ReverseWord.java
@@ -1,14 +1,11 @@
 public class ReverseWord {
-
   public static void main(String[] args) {
     String word = "hello"; // Replace with your desired word
-
     // Reverse the word
     StringBuilder reversedWord = new StringBuilder();
     for (int i = word.length() - 1; i >= 0; i--) {
       reversedWord.append(word.charAt(i));
     }
-
     // Print the reversed word
     System.out.println(reversedWord);
   }
